fa68567eb238b4cf37e81811870178e3307d0b4a
willian-mayer <willian.mayer.br@gmail.com>
2025-05-23 10:59:54 -0300

    installing pages, page components and integrating React Router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import routesJson from './data/routes.json'
+import { Routes, Route } from 'react-router-dom'
+import Services from './pages/Services'
+import AboutUs from './pages/AboutUs'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
       <Navbar title="Lynx3PL" routes={routesJson}/>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/services' element={<Services />}/>
+          <Route path='/about-us' element={<AboutUs />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </main>
     </>
   )
 }
