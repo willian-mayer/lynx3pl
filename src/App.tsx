da3ca70ce1c95a1a9f049673a25ae4bb537d3a67
willian-mayer <willian.mayer.br@gmail.com>
2025-05-23 10:18:49 -0300

    adding navbar, logo, and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import routesJson from './data/routes.json'
 
 function App() {
   return (
     <>
+      <Navbar title="Lynx3PL" routes={routesJson}/>
       <h1>Hello world!</h1>
     </>
   )
